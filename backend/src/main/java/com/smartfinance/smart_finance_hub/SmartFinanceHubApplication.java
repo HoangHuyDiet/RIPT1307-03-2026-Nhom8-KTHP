2fae5c719248ba7e24942f65f619d3df684c49e8
HoangHuyDiet <hoangnguyen123425@gmail.com>
2026-05-15 23:23:07 +0700

    first commit

---
+++ backend/src/main/java/com/smartfinance/smart_finance_hub/SmartFinanceHubApplication.java
@@ -0,0 +1,13 @@
+package com.smartfinance.smart_finance_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinanceHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartfinance/smart_finance_hub/SmartFinanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfinance.smart_finance_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
